d20b7d6039574d6552c8d89c83a201a889662582
ssimkani <seenasimkani@gmail.com>
2024-06-10 11:31:16 -0400

    Add node.rb and linked_list.rb to lib folder

---
+++ lib/linked_list.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require_relative 'node'
+
+class LinkedList
+  attr_accessor :size
+
+  def initialize
+    @head = nil
+    @size = 0
+  end
+
+  def prepend(value)
+    @head = Node.new(value, @head)
+    @size += 1
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+    else
+      node = @head
+      node = node.next_node while node.next_node
+      node.next_node = new_node
+    end
+    @size += 1
+  end
+
+  def at(index)
+    index += size if index.negative?
+    return nil if index >= @size || @size.zero?
+
+    current = @head
+    index.times { current = current.next_node }
+    current.value
+  end
+
+  def pop
+    node = @head
+    (size - 2).times { node = node.next_node }
+    popped_node = node.next_node
+    node.next_node = nil
+    @size -= 1
+    popped_node.value
+  end
+
+  def contains?(value)
+    current_node = @head
+    until current_node.nil?
+      return true if current_node.value == value
+
+      current_node = current_node.next_node
+    end
+    false
+  end
+
+  def find(value)
+    current_node = @head
+    index = 0
+    (size - 1).times do
+      return index if current_node.value == value
+
+      current_node = current_node.next_node
+      index += 1
+    end
+    nil
+  end
+
+  def insert_at(value, index)
+    index += size + 1 if index.negative?
+    return unless index <= @size && index >= 0
+
+    if index.zero?
+      prepend(value)
+    elsif index == @size
+      append(value)
+    else
+      node = Node.new(value)
+      current_node = @head
+      (index - 1).times { current_node = current_node.next_node }
+      node.next_node = current_node.next_node
+      current_node.next_node = node
+    end
+
+    @size += 1
+  end
+
+  def remove_at(index)
+    index += size if index.negative?
+    return if index >= @size || @size.zero?
+
+    if index.zero?
+      @head = @head.next_node
+    else
+      current_node = @head
+      (index - 1).times { current_node = current_node.next_node }
+      current_node.next_node = current_node.next_node.next_node
+    end
+    @size -= 1
+  end
+
+  def to_s
+    current = @head
+    until current.nil?
+      print "( #{current.value} ) -> "
+      current = current.next_node
+    end
+    puts 'nil'
+  end
+
+  def head
+    @head.value
+  end
+
+  def tail
+    current = @head
+    current = current.next_node until current.next_node.nil?
+    current.value
+  end
+end
