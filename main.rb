bceb55c0ce964a387065d223fd2249470b3afe1e
ssimkani <seenasimkani@gmail.com>
2024-06-10 11:31:41 -0400

    Create linked_list object in main.rb

---
+++ main.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'lib/linked_list'
+
+linked_list = LinkedList.new
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.append(4)
+linked_list.append(5)
+linked_list.prepend(0)
+linked_list.insert_at(6, 3)
+linked_list.remove_at(3)
+linked_list.to_s
